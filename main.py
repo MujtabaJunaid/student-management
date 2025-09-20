733a20e45568d1d78bafffb34067cd2c2f646afc
MujtabaJunaid <mujtabajunaid412@gmail.com>
2025-09-20 17:52:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://mujtabajunaid.github.io/student-management/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
