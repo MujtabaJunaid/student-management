d2224c1ea6e196d62302f1ac74919b3c67ac3460
MujtabaJunaid <mujtabajunaid412@gmail.com>
2025-09-20 14:22:31 +0500

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,8 +1,11 @@
-import { reportWebVitals } from 'web-vitals';
+import { reportWebVitals as reportVitalsFromWebVitals } from 'web-vitals';
 
 const reportWebVitals = (metric) => {
   console.log(metric);  // You can replace this with custom logging or analytics
 };
 
-// Make sure to use the default export
+// Call the imported reportWebVitals function with your custom function
+reportVitalsFromWebVitals(reportWebVitals);
+
+// Export the custom function as the default export
 export default reportWebVitals;
